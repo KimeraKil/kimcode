e4c1b0f5c58866a69b69fe5da75aa85301003315
KimeraKil <99593857+KimeraKil@users.noreply.github.com>
2023-08-21 11:06:41 -0300

    Add files via upload

--- js/kim.js
+++ js/kim.js
@@ -1,3 +1,28 @@
 function detalhes(id){
-    alert (id)
+    //alert (id)
+    $.ajax({
+        url:"./config/informaDetal.php",
+        type: "POST",
+        data:{
+            acao: 'detalhes', id:id
+        },
+        success:(result)=>{
+            eVerificado = JSON.parse(result);
+            //console.log(eVerificado)
+            $('#titulo').html(eVerificado['titulo'])
+            //$('#f1').html(eVerificado['img1'])
+            //$('#f2').html(eVerificado['img2'])
+            //$('#f3').html(eVerificado['img3'])
+            $('#sis').html(eVerificado['sistema'])
+            $('#req').html(eVerificado['requisito'])
+            $('#valor').html(eVerificado['preco'])
+            $('#detail').html(eVerificado['detalhes'])
+            $('#obs').html(eVerificado['observacao'])
+            $('.detalhes').toggleClass('some')
+        }
+    })
 }
+
+$('#btnClose').on('click', function(){
+    $('.detalhes').toggleClass('some')
+})
